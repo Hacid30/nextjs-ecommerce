fcac0a1c0055dbc1afee3b87997d1bdb05dc8079
Hacid30 <hectorhacid@gmail.com>
2026-08-16 22:19:35 -0500

    fix: resolve prerender build error on vercel by disabling cache on fetch

--- src/app/catalogo/page.js
+++ src/app/catalogo/page.js
@@ -2,7 +2,8 @@ import ProductCard from '../../components/ProductCard';
 
 async function getProducts() {
     
-    const res = await fetch('https://fakestoreapi.com/products');
+    const res = await fetch('https://fakestoreapi.com/products', 
+        { cache: 'no-store'});
     
     if (!res.ok) {
         throw new Error('Error al obtener los productos');
